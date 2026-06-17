dc27dbfa5300ce9f3cc40ca06df5474b271b17dd
libin50 <libin50@lenovo.com>
2026-06-17 17:24:40 +0800

    add token save key

--- lib/common/constants/local_key_constants.dart
+++ lib/common/constants/local_key_constants.dart
@@ -4,4 +4,11 @@ class LocalKeyConstants {
 
   /// token 保存 key
   static const String accountToken = 'bagel_token';
+
+  /// 当前登录账号保存 key
+  ///
+  /// 这里保存的是用户输入的手机号 / 邮箱。
+  /// 后面如果服务端返回用户信息，比如 userId、nickname、avatar，
+  /// 我们再继续加 key。
+  static const String currentAccount = 'bagel_current_account';
 }
